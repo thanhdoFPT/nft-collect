527686e90ea079cc55ce956ecf3a4f25db1a6345
thanhdoFPT <132207620+thanhdoFPT@users.noreply.github.com>
2023-05-20 23:59:08 +0700

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -42,7 +42,7 @@ const Banner = () => {
           </div>
         </div>
 
-        <motion.div variants={fadeIn('up', 0.6)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='hidden lg:flex flex-1'>
+        <motion.div variants={fadeIn('up', 0.6)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='hidden lg:flex flex-1 p-7'>
           <img  className='shadow-2xl  w-full h-full object-cover rounded-md' src={Images} alt='' />
         </motion.div>
       </div>
