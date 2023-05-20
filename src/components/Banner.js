256fe91ba4e9ec78c7902b3b2cfcc517d1a8c773
thanhdoFPT <132207620+thanhdoFPT@users.noreply.github.com>
2023-05-20 23:52:46 +0700

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,7 +9,7 @@ import { fadeIn } from '../variants'
 const Banner = () => {
   return <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
     <div className='container mx-auto'>
-      <div className='flex flex-col justify-between lg:flex-row lg:items-center lg:gap-x-12'>
+      <div className='flex flex-col justify-between lg:flex-row items-start lg:gap-x-12'>
         <div>
           <div className='flex-1 text-center font-secondary lg:text-left gap-2'>
             <motion.h1 variants={fadeIn('up', 0.4)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='text-[55px] font-bold leading-[0.8] lg:text-[110px] mb-5'>
