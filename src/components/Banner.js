c300d5972c181ed84b2c5d6d64473789ba536050
thanhdoFPT <132207620+thanhdoFPT@users.noreply.github.com>
2023-05-20 23:51:35 +0700

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ const Banner = () => {
     <div className='container mx-auto'>
       <div className='flex flex-col justify-between lg:flex-row lg:items-center lg:gap-x-12'>
         <div>
-          <div className='flex-1 text-center font-secondary lg:text-left'>
+          <div className='flex-1 text-center font-secondary lg:text-left gap-2'>
             <motion.h1 variants={fadeIn('up', 0.4)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='text-[55px] font-bold leading-[0.8] lg:text-[110px] mb-5'>
               
             </motion.h1>
@@ -42,7 +42,7 @@ const Banner = () => {
           </div>
         </div>
 
-        <motion.div variants={fadeIn('up', 0.6)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[420px]'>
+        <motion.div variants={fadeIn('up', 0.6)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className='hidden lg:flex flex-1'>
           <img  className='shadow-2xl   w-[420px]  ' src={Images} alt='' />
         </motion.div>
       </div>
