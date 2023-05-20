cb12d55eefce4af3574a283d7bfbb91454fe7d49
thanhdoFPT <132207620+thanhdoFPT@users.noreply.github.com>
2023-05-20 23:47:15 +0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
   return <section className='mt-10 section' id='about' ref={ref} >
     <div className='container mx-auto shadow-2xl'>
       <div className='relative flex flex-col h-screen gap-x-20 lg:flex-row lg:items-center lg:gap-y-10 lg:gap-y-0'>
-        <motion.div variants={fadeIn('right', 0.3)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.3 }} className='flex-1 bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top mt-20' >
+        <motion.div variants={fadeIn('right', 0.3)} initial="hidden" whileInView={'show'} viewport={{ once: false, amount: 0.3 }} className='flex-1 bg-contain bg-no-repeat h-[640px] bg-top mt-20' >
           <p className='mb-5 ml-2 text-4xl md:text-6xl xl:text-6xl text-gradient btn-link'>CAFEY NFT SUI</p>
           <p className='mb-10 ml-2 text-sm md:text-xl xl:text-2xl text-gradient btn-link'>The 'Cafey NFT' Collection</p>
           <img className=' h-2/3 rounded-2xl' src={aboutNew} />
